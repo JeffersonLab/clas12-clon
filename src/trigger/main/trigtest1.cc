3560b077087159e87d4561ad3f13aeb5d614fe60
Rafayel Paremuzyan <rafopar@jlab.org>
2018-01-28 23:37:45 -0500

    asdasd

--- src/trigger/main/trigtest1.cc
+++ src/trigger/main/trigtest1.cc
@@ -16,13 +16,13 @@
 
 /*******************************/
 /* uncomment desired detectors */ 
-#define USE_ECAL
-#define USE_PCAL
-#define USE_HTCC
-#define USE_FTOF
-#define USE_CTOF
-#define USE_CND
-#define USE_FT
+// #define USE_ECAL
+ #define USE_PCAL
+//#define USE_HTCC
+//#define USE_FTOF
+//#define USE_CTOF
+//#define USE_CND
+//#define USE_FT
 
 
 
@@ -63,7 +63,7 @@ static uint16_t ec_dalitzmax = (74<<3)/*EC_DALITZ_MAX*/;
 static uint16_t ec_nstripmax = 0;
 
 static uint16_t pc_threshold[3] = { 1, 1, 3 };
-static uint16_t pc_nframes = 0;
+static uint16_t pc_nframes = 3;
 static uint16_t pc_dipfactor = PC_STRIP_DIP_FACTOR;
 static uint16_t pc_dalitzmin = PC_DALITZ_MIN;
 static uint16_t pc_dalitzmax = PC_DALITZ_MAX;
