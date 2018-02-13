97c8214f80a214e85b7d39d2815b51510ca9b8ba
Sergey Boyarinov <boiarino17@gmail.com>
2018-02-12 19:44:24 -0500

    main

--- src/trigger/main/trigtest1.cc
+++ src/trigger/main/trigtest1.cc
@@ -17,14 +17,14 @@
 
 /*******************************/
 /* uncomment desired detectors */ 
-//#define USE_ECAL
-//#define USE_PCAL
-//#define USE_HTCC
-//#define USE_FTOF
-//#define USE_CTOF
+#define USE_ECAL
+#define USE_PCAL
+#define USE_HTCC
+#define USE_FTOF
+#define USE_CTOF
 #define USE_CND
-//#define USE_FT
-//#define USE_PCU
+#define USE_FT
+#define USE_PCU
 
 
 
