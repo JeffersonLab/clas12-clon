014a22c94d796a98a690bd7fd58376e5081892c3
Sergey Boyarinov <boiarino17@gmail.com>
2019-12-04 15:01:47 -0500

    work around translation table fake channels in ecstrips.cc

--- src/trigger/ec.s/ecstrips.cc
+++ src/trigger/ec.s/ecstrips.cc
@@ -386,45 +386,48 @@ ecstrips(ap_uint<16> strip_threshold, hls::stream<fadc_4ch_t> s_fadc_words[NFADC
 #ifdef DEBUG
       cout<<"fadcs[slot="<<isl<<"][read="<<j<<"]="<<fadcs.e0<<" "<<fadcs.e1<<" "<<fadcs.e2<<" "<<fadcs.e3<<endl;
 #endif
-
+      if( isl==(nslot-1) && j==(NH_READS-1) ) /* skip last 4 channels of last slot */
 	  {
-      energy = fadcs.e0;
-      chan = j*NH_READS;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0); /* error in '?' without (uint16_t) ...*/
+		;
+	  }
+	  else
+	  {
+        energy = fadcs.e0;
+        chan = j*NH_READS;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0); /* error in '?' without (uint16_t) ...*/
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e0="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e0="<<energytmp[layer][str]<<endl;
 #endif
 
-      energy = fadcs.e1;
-      chan = j*NH_READS+1;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
+        energy = fadcs.e1;
+        chan = j*NH_READS+1;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e1="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e1="<<energytmp[layer][str]<<endl;
 #endif
 
-      energy = fadcs.e2;
-      chan = j*NH_READS+2;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
+        energy = fadcs.e2;
+        chan = j*NH_READS+2;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e2="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e2="<<energytmp[layer][str]<<endl;
 #endif
 
-      energy = fadcs.e3;
-      chan = j*NH_READS+3;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
+        energy = fadcs.e3;
+        chan = j*NH_READS+3;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] = ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e3="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e3="<<energytmp[layer][str]<<endl;
 #endif
 	  }
-
 
 	  /* for ECAL only, add outer part to inner; translation table the same but slots 7-13 (inner 0-6) */
 #ifndef USE_PCAL
@@ -434,41 +437,49 @@ ecstrips(ap_uint<16> strip_threshold, hls::stream<fadc_4ch_t> s_fadc_words[NFADC
       cout<<"fadcs[slot="<<isl<<"][read="<<j<<"]="<<fadcs.e0<<" "<<fadcs.e1<<" "<<fadcs.e2<<" "<<fadcs.e3<<endl;
 #endif
 
-      energy = fadcs.e0;
-      chan = j*NH_READS;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0); /* error in '?' without (uint16_t) ...*/
+      if( isl==(nslot-1) && j==(NH_READS-1) ) /* skip last 4 channels of last slot */
+	  {
+		;
+	  }
+	  else
+	  {
+        energy = fadcs.e0;
+        chan = j*NH_READS;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0); /* error in '?' without (uint16_t) ...*/
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e0="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e0="<<energytmp[layer][str]<<endl;
 #endif
 
-      energy = fadcs.e1;
-      chan = j*NH_READS+1;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
+        energy = fadcs.e1;
+        chan = j*NH_READS+1;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e1="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e1="<<energytmp[layer][str]<<endl;
 #endif
 
-      energy = fadcs.e2;
-      chan = j*NH_READS+2;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
+        energy = fadcs.e2;
+        chan = j*NH_READS+2;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e2="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e2="<<energytmp[layer][str]<<endl;
 #endif
 
-      energy = fadcs.e3;
-      chan = j*NH_READS+3;
-      layer = adclayerecal[isl][chan] - 1; /* from 0 */
-      str   = adcstripecal[isl][chan] - 1; /* from 0 */
-      energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
+        energy = fadcs.e3;
+        chan = j*NH_READS+3;
+        layer = adclayerecal[isl][chan] - 1; /* from 0 */
+        str   = adcstripecal[isl][chan] - 1; /* from 0 */
+        energytmp[layer][str] += ((energy >= strip_threshold) ? ((uint16_t)energy) : 0);
 #ifdef DEBUG
-      cout<<"   layer="<<layer<<" str="<<str<<" -> e3="<<energytmp[layer][str]<<endl;
+        cout<<"   layer="<<layer<<" str="<<str<<" -> e3="<<energytmp[layer][str]<<endl;
 #endif
+	  }
+
 
 #endif /*ifndef USE_PCAL*/
 
